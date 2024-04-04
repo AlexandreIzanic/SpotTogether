7ca9d875ab3a8507f36dcb582e5bc4261713d836
AlexandreIzanic <alexandre.izanic@gmail.com>
2024-04-04 23:12:50 +0200

    feat: popup

--- app/src/components/Popup.jsx
+++ app/src/components/Popup.jsx
@@ -1,8 +1,16 @@
+import { FaInstagram } from "react-icons/fa";
+
 const Popup = ({ place, setSelectedMarker }) => {
   if (!place) return null;
+
   return (
-    <div className=" text-white  rounded-xl w-[20vw] bg-[#2D2D2D] ">
+    <div className=" text-white  rounded-xl w-[20vw] bg-[#7480ff] p-4 ">
       <div className="font-bold text-xl">{place.title}</div>
+      {place.instagram_url && (
+        <a href={place.instagram_url} target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      )}
       <button onClick={() => setSelectedMarker(null)}> Unmark</button>
     </div>
   );
